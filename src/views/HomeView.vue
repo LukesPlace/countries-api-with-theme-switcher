b7271cddfa42230466baba96807707c6ad230efb
Luke Mason <Lukemason161@gmail.com>
2023-07-15 23:07:40 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div>
+      <h2>Where in the world?</h2>
+      <img>
+      <h3>Dark Mode</h3>
+    </div>
+    <TheWelcome />
+  </main>
+</template>
